3316cb1ccd1f585e33b0b0e471c3114d0a29f887
lear23 <130910243+lear23@users.noreply.github.com>
2024-03-06 11:12:36 +0100

    Fixade database med Identity

--- IdentityAspNetMvc/Program.cs
+++ IdentityAspNetMvc/Program.cs
@@ -1,12 +1,15 @@
+using Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("LocalDatabase")));
+
+
+
+
 
 var app = builder.Build();
-
-
-
-
-
 app.UseHsts();
 app.UseStatusCodePagesWithReExecute("/error", "?satusCode={0}");
 app.UseHttpsRedirection();
---
+++ Infrastructure/Factories/AppDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Infrastructure.Contexts;
+
+namespace Infrastructure.Factories;
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        optionsBuilder.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\SkolaUppgifter\ASP.NET\IdentityAspNetMvc\Infrastructure\Data\Local_database.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=True");
+
+        return new AppDbContext(optionsBuilder.Options);
+    }
+}
