a66cd4141ee8780550c221275895b1eedc30e720
lear23 <130910243+lear23@users.noreply.github.com>
2024-03-29 11:43:26 +0100

    fixad dark,light och CookiesConsent

---
+++ IdentityAspNetMvc/Controllers/SiteSettings.cs
@@ -0,0 +1,39 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System;
+
+namespace SiliconWebApp.Controllers
+{
+    public class SiteSettingsController : Controller
+    {
+        public IActionResult ChangeTheme(string mode)
+        {
+            var option = new CookieOptions
+            {
+                Expires = DateTime.Now.AddDays(60),
+            };
+            Response.Cookies.Append("ThemeMode", mode, option);
+
+            return Ok();
+        }
+
+        [HttpPost]
+
+        public IActionResult CookieConsent()
+        {
+            var option = new CookieOptions
+            {
+                Expires = DateTime.Now.AddMinutes(5),
+                HttpOnly = true,
+                Secure = true
+            };
+            Response.Cookies.Append("CookieConsent","true" , option);
+            return Ok();    
+        }
+
+    }   
+
+
+}
+
--- IdentityAspNetMvc/wwwroot/js/site.js
+++ IdentityAspNetMvc/wwwroot/js/site.js
@@ -22,3 +22,32 @@ window.addEventListener("resize", () => {
         abrir.style.display = "block";
     }
 });
+
+
+/* -------Dark Mode & Light Mode-------------------*/
+
+document.addEventListener('DOMContentLoaded', function () {
+    let sw = document.querySelector('#switch-mode');
+
+    sw.addEventListener('change', function () {
+        let theme = this.checked ? "dark" : "light";
+
+        fetch(`/sitesettings/changetheme?mode=${theme}`)
+            .then(res => {
+                if (res.ok) {
+                    window.location.reload();
+                } else {
+                    console.log('Something went wrong while processing the request.');
+                }
+            })
+            .catch(error => {
+                console.error('Error making the AJAX request:', error);
+            });
+    });
+
+    let themeMode = document.cookie.replace(/(?:(?:^|.*;\s*)ThemeMode\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+    let switchMode = document.getElementById('switch-mode');
+    if (themeMode === "dark") {
+        switchMode.checked = true;
+    }
+});
